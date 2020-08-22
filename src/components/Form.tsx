f734e2a88144ebc1c64e4a3eb7c55e3043d99065
Ilya Paripsa <iparips@gmail.com>
2020-08-22 14:38:18 +1000

    Make the search respect search terms

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,7 @@ import fetchJsonp from 'fetch-jsonp';
 import {Image} from "../types";
 import {mapResponseToImages} from "../mapper/mapResponseToImages";
 
-const FLICKR_PUBLIC_FEED_URL = 'https://www.flickr.com/services/feeds/photos_public.gne?tags=cats&format=json';
+const FLICKR_PUBLIC_FEED_URL = 'https://www.flickr.com/services/feeds/photos_public.gne?format=json';
 
 type PropTypes = {
     setSearchResults: Dispatch<SetStateAction<Image[]>>
@@ -14,7 +14,8 @@ export const Form = ({setSearchResults}: PropTypes) => {
     const [searchTerm, setSearchTerm] = useState<string>('');
 
     useEffect(() => {
-        fetchJsonp(FLICKR_PUBLIC_FEED_URL, {jsonpCallback: 'jsoncallback'})
+        const url = `${FLICKR_PUBLIC_FEED_URL}&tags=${encodeURI(searchTerm)}`;
+        fetchJsonp(url, {jsonpCallback: 'jsoncallback'})
             .then(async res => {
                 const responseJson = await res.json();
                 const images: Image[] = mapResponseToImages(responseJson);
