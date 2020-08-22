f4c42c9cad3d7c88a3d86d1f7eabb83475e1dd89
Ilya Paripsa <iparips@gmail.com>
2020-08-23 07:44:32 +1000

    Explaining hardcoding of image dimensions

--- src/mapResponseToImages.ts
+++ src/mapResponseToImages.ts
@@ -3,6 +3,12 @@ import {FlickrItem, FlickrResponse, Image} from "./types";
 const mapFlickrItemToImage = ({author, media, tags}: FlickrItem): Image => {
     return {
         author,
+        // Hard-coding dimensions of images below as a shortcut
+        // to meet the requirements of react-photo-gallery dependency
+        // to make the presentation of images look ok.
+        // Although whether they actually look ok is arguable
+        // because these are not true dimensions of the images,
+        // so some stretching does take place.
         width: 1,
         height: 1,
         tags: tags.split(' '),
