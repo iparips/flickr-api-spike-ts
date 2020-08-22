47e3e206fd2b205b339ac14d7e15e2a20751d750
Ilya Paripsa <iparips@gmail.com>
2020-08-22 16:25:56 +1000

    Add useDebounce

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent, Dispatch, SetStateAction, useEffect, useState} from 
 import fetchJsonp from 'fetch-jsonp';
 import {Image} from "../types";
 import {mapResponseToImages} from "../mapper/mapResponseToImages";
+import {useDebounce} from 'use-debounce';
 
 const FLICKR_PUBLIC_FEED_URL = 'https://www.flickr.com/services/feeds/photos_public.gne?format=json';
 
@@ -12,21 +13,22 @@ type PropTypes = {
 export const Form = ({setSearchResults}: PropTypes) => {
 
     const [searchTerm, setSearchTerm] = useState<string>('cats');
+    const [debouncedSearchTerm] = useDebounce(searchTerm, 100);
 
     useEffect(() => {
-        const url = `${FLICKR_PUBLIC_FEED_URL}&tags=${encodeURI(searchTerm)}`;
+        if (debouncedSearchTerm === '') {return;}
+        const url = `${FLICKR_PUBLIC_FEED_URL}&tags=${encodeURI(debouncedSearchTerm)}`;
         fetchJsonp(url, {jsonpCallback: 'jsoncallback'})
             .then(async res => {
                 const responseJson = await res.json();
                 const images: Image[] = mapResponseToImages(responseJson);
                 setSearchResults(images);
             });
-    }, [searchTerm]);
+    }, [debouncedSearchTerm]);
 
     return <form className="searchBox">
         <label className="searchBox__label" htmlFor="searchTerm">Image search</label>
-        <input type="text" name="searchTerm" id={searchTerm}
+        <input type="text" name="searchTerm" id={searchTerm} value={searchTerm}
                onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}/>
-
     </form>
 };
