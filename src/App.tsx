fbea7da465c5c0a4fe874c4576eb313585c75dd1
Ilya Paripsa <iparips@gmail.com>
2020-08-22 16:28:42 +1000

    Move css outside of the project root

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import './App.css';
+import './css/App.css';
 import {Form} from "./components/Form";
 import {Image} from "./types";
 import Gallery from "react-photo-gallery";
